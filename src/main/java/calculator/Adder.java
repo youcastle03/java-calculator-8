9a0153b56228bbaf613ac68991ee876302e34014
youcastle03 <luckthb325@naver.com>
2025-10-20 00:56:33 +0900

    feat:덧셈 구현

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -1,4 +1,14 @@
 package calculator;
 
+import java.util.List;
+
 public class Adder {
+
+    public int addNum(List<Integer> numbers) {
+        int sum = 0;
+        for(int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
 }
